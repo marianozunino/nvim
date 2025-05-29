42ce37c7a1e0f64494ecee09d04cdaf557afc3b4
Mariano Z. <marianoz@posteo.net>
2025-05-29 12:06:56 -0300

    dev: automated commit - 2025-05-29 12:06:56

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,8 +1,8 @@
 local M = {
-  "mson-org/mason-lspconfig.nvim",
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
   dependencies = {
-    "mson-org/mason.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    "mason-org/mason.nvim",
     "j-hui/fidget.nvim",
     "ibhagwan/fzf-lua",
     require("plugins.lsp.extras.lazydev"),
@@ -10,36 +10,64 @@ local M = {
   },
 }
 
+local servers = {
+  "gopls",
+  "jsonls",
+  "lua_ls",
+  "yamlls",
+  "graphql",
+  "html",
+  "omnisharp",
+  "svelte",
+  "vtsls",
+  "ccls",
+  "templ",
+}
+
+local tools = {
+  "prettierd",
+  "shfmt",
+  "stylua",
+  "latexindent",
+  "clang-format",
+  "csharpier",
+  "quick-lint-js",
+}
+
 local function setup_keymaps(bufnr)
-  local opts = { buffer = bufnr, noremap = true, silent = true }
-  local keymaps = {
-    { "K", vim.lsp.buf.hover, desc = "Hover Doc", border = "rounded" },
-    { "<C-h>", vim.lsp.buf.signature_help, desc = "Signature Help", border = "rounded" },
-    { "<leader>rn", vim.lsp.buf.rename, desc = "Rename" },
-    { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action" },
-    { "gd", require("fzf-lua").lsp_definitions, desc = "Go to Definition" },
-    { "gr", require("fzf-lua").lsp_references, desc = "Go to References" },
-    { "gD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
-    { "gi", require("fzf-lua").lsp_implementations, desc = "Go to Implementation" },
-    { "gt", require("fzf-lua").lsp_typedefs, desc = "Go to Type Definition" },
-    { "<leader>vd", vim.diagnostic.open_float, desc = "View Diagnostics" },
-    { "<leader>dl", require("fzf-lua").diagnostics_document, desc = "Document Diagnostics" },
-    { "<leader>dw", require("fzf-lua").diagnostics_workspace, desc = "Workspace Diagnostics" },
-    { "<leader>ds", require("fzf-lua").lsp_document_symbols, desc = "Document Symbols" },
-    { "<leader>ws", require("fzf-lua").lsp_workspace_symbols, desc = "Workspace Symbols" },
-    { "<leader>lr", ":LspRestart<CR>", desc = "Restart LSP" },
-    { "<leader>li", ":LspInfo<CR>", desc = "LSP Info" },
-  }
-  for _, map in ipairs(keymaps) do
-    nmap(map[1], map[2], vim.tbl_extend("force", opts, { desc = map[3], border = map[4] }))
-  end
+  local fzf = require("fzf-lua")
+  local opts = { buffer = bufnr }
+
+  -- Basic LSP
+  nmap("K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover Doc" }))
+  nmap("<C-h>", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, { desc = "Signature Help" }))
+  nmap("<leader>r", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename" }))
+  nmap("<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
+
+  -- Navigation
+  nmap("gd", fzf.lsp_definitions, vim.tbl_extend("force", opts, { desc = "Go to Definition" }))
+  nmap("gr", fzf.lsp_references, vim.tbl_extend("force", opts, { desc = "Go to References" }))
+  nmap("gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "Go to Declaration" }))
+  nmap("gi", fzf.lsp_implementations, vim.tbl_extend("force", opts, { desc = "Go to Implementation" }))
+  nmap("gt", fzf.lsp_typedefs, vim.tbl_extend("force", opts, { desc = "Go to Type Definition" }))
+
+  -- Diagnostics
+  nmap("vd", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "View Diagnostics" }))
+  nmap("<leader>dl", fzf.diagnostics_document, vim.tbl_extend("force", opts, { desc = "Document Diagnostics" }))
+  nmap("<leader>dw", fzf.diagnostics_workspace, vim.tbl_extend("force", opts, { desc = "Workspace Diagnostics" }))
+  nmap("<leader>ds", fzf.lsp_document_symbols, vim.tbl_extend("force", opts, { desc = "Document Symbols" }))
+  nmap("<leader>ws", fzf.lsp_workspace_symbols, vim.tbl_extend("force", opts, { desc = "Workspace Symbols" }))
+
+  -- LSP management
+  nmap("<leader>lr", ":LspRestart<CR>", vim.tbl_extend("force", opts, { desc = "Restart LSP" }))
+  nmap("<leader>li", ":LspInfo<CR>", vim.tbl_extend("force", opts, { desc = "LSP Info" }))
 end
 
 function M.config()
   require("mason").setup({ max_concurrent_installers = 4 })
   require("fidget").setup({})
 
-  -- Diagnostic configuration
+  -- Diagnostics
   vim.diagnostic.config({
     virtual_text = { spacing = 2, source = "if_many" },
     float = { border = "rounded", source = "if_many" },
@@ -54,51 +82,17 @@ function M.config()
     } or {},
   })
 
-  -- List of servers to enable (matching files in ~/.config/nvim/lsp/)
-  local servers = {
-    "gopls",
-    "jsonls",
-    "lua_ls",
-    "yamlls",
-    "graphql",
-    "html",
-    "omnisharp",
-    "svelte",
-    "vtsls",
-    "ccls",
-    "templ",
-  }
-
-  -- Servers not supported by mason-lspconfig
-  local mason_unsupported = { "ccls" }
-
-  -- Filter supported servers for mason-lspconfig
-  local mason_servers = vim.tbl_filter(function(server)
-    return not vim.tbl_contains(mason_unsupported, server)
+  -- Mason setup
+  local mason_servers = vim.tbl_filter(function(s)
+    return s ~= "ccls"
   end, servers)
-
-  -- Tools
-  local tools = {
-    "prettierd",
-    "shfmt",
-    "stylua",
-    "latexindent",
-    "clang-format",
-    "csharpier",
-    "quick-lint-js",
-  }
-
-  -- Mason-LSPconfig setup for automatic installation
   require("mason-lspconfig").setup({
     ensure_installed = mason_servers,
     automatic_installation = true,
     automatic_enable = true,
   })
-
-  -- Mason tool installer for tools and ccls
   require("mason-tool-installer").setup({ ensure_installed = tools })
 
-  -- Enable LSP servers
   vim.lsp.enable(servers)
 
   -- LSP Attach
@@ -106,25 +100,27 @@ function M.config()
     group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
     callback = function(args)
       local client = vim.lsp.get_client_by_id(args.data.client_id)
-      local bufnr = args.buf
+      if not client then
+        return
+      end
 
-      setup_keymaps(bufnr)
+      setup_keymaps(args.buf)
 
       -- Inlay hints
-      if client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-        vim.keymap.set("n", "<leader>th", function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
-        end, { buffer = bufnr, desc = "Toggle Inlay Hints" })
+      if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, args.buf) then
+        nmap("<leader>th", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf }), { bufnr = args.buf })
+        end, { buffer = args.buf, desc = "Toggle Inlay Hints" })
       end
 
       -- Document highlights
       if client.server_capabilities.documentHighlightProvider then
         vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-          buffer = bufnr,
+          buffer = args.buf,
           callback = vim.lsp.buf.document_highlight,
         })
         vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-          buffer = bufnr,
+          buffer = args.buf,
           callback = vim.lsp.buf.clear_references,
         })
       end
